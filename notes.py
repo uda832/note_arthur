18f0dc3c8a9d6136bff8a203a840c3b6938aa1e6
RBhuyan <33669083+RBhuyan@users.noreply.github.com>
2018-03-29 12:22:00 -0400

    Delete notes.py

--- notes.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from app.models import User, Section, Note
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Section': Section, 'Note': Note}
