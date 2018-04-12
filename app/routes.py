b3f89518155aadfb8398cd6ea5d0338838faf8a7
RBhuyan <33669083+RBhuyan@users.noreply.github.com>
2018-04-12 18:59:09 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
-from flask import render_template, flash, redirect, url_for, request
-from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from flask import Flask, request, Response, json, jsonify, render_template, flash, redirect, url_for
+from urllib.parse import unquote
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from flask_login import UserMixin
+from app.models import User, Section
 from werkzeug.urls import url_parse
+from sqlalchemy import text
+from werkzeug.security import generate_password_hash, check_password_hash
+import jinja2
+import json
+
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, SectionForm
 
 
 @app.route('/')
@@ -21,7 +28,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('arthur.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,12 +36,18 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+    con = db.engine.connect()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+        user = con.execute('SELECT * FROM User WHERE username=="{0}"'.format(form.username.data))
+        theUser = user.fetchone()
+        if not theUser:
+            flash("Invalid username or password")
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
+        if not check_password_hash(theUser[3],form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        loginUser = User.query.filter_by(username=theUser[1]).first()
+        login_user(loginUser,remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -47,17 +60,171 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/arthur', methods=['GET', 'POST'])
+def main_arthur():
+    '''
+        Main page:
+        - should be only accessible after the user successfully authenticates (signs in)
+    '''
+    #json = buildJSONstring()
+    return render_template('arthur.html', json=json)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
+    '''if current_user.is_authenticated:
+        return redirect(url_for('index'))'''
     form = RegistrationForm()
+    con = db.engine.connect()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
+        '''user = User(username=form.username.data, email=form.email.data)'''
+        '''user.set_password(form.password.data)'''
+        username=form.username.data
+        email=form.email.data
+        password_hash=generate_password_hash(form.password.data)
+        '''db.session.add(user)'''
+        con.execute('INSERT INTO User (username,email,password_hash) VALUES ("{0}","{1}","{2}")'.format(username,email,password_hash))
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
+    ''' Test function to see if we can insert a new note+section '''
+    form = SectionForm()
+    con = db.engine.connect()
+    if form.validate_on_submit():
+        id = current_user.get_id()
+        body = form.body.data
+        con.execute('INSERT INTO Section(body, user_id) VALUES ("{0}", {1})'.format(body, id))
+        db.session.commit()
+        flash('Successfully created a new section!')
+        return redirect(url_for('login'))
+    return render_template('insert.html', title = 'Insert', form=form)
+
+
+
+
+@app.route('/save', methods=['POST'])
+def save_request():
+    '''
+        Save request handler:
+        - Takes the client's AJAX request and saves the content of the DataStore to the DB
+    '''
+    dsJSON = request.form["json"]
+    dsJSON = unquote(dsJSON)
+    ds = json.loads(dsJSON)     # Build the DataStore object from the JSON
+    
+
+    # Note to Bo, Rohan, etc. (everyone that's setting up the DB access logic)
+    #   At this point, you can access the exact DataStore object which contains the new notes 
+    #   I've written a demo function that iterates over the DataStore and accesses each information
+    #   Please reference this and write the function to run the INSERT sql statements
+    #update_db_from_datastore_demo(ds)
+
+    return "success"
+"""
+def update_db_from_datastore_demo(ds):
+    '''
+        Example function to demonstrate how we will access the DataStore object
+    '''
+    user_name = "Uda Yeruultsengel" # Note: this should probably be set somewhere globally for each request
+    
+    for section in ds:
+        # Check if the current section already exists
+        my_sql = '''
+            SELECT TOP 1 FROM dataSections A INNER JOIN dataUsers as B ON A.uID = B.uID
+            WHERE A.uID = ''' + user_name + ''' AND A.Title = ''' + section['title'] + '''
+            '''
+        # Note: this is a pseudo function 
+        section_exists = check_if_sql_returns_a_record(my_sql)
+
+        # Section already exists. So iterate over notes and perform UPDATE statements
+        if section_exists:
+            for note in section['notes']:
+                # Perform the same check for notes 
+
+                # If it already exists, perform UPDATE statement
+
+                # Else, if it it doesn't exist, create a new record
+
+        # Section does not exist. Run a INSERT INTO statement
+        else:
+            my_sql_1 = '''
+                DECLARE @NEXTKEY as int = ''' + grab_next_key() + '''
+                DECLARE @title as nvarchar(max) = ''' + section['title'] +'''
+                DECLARE @uid as int = ''' + grab_user_pk() +'''
+                DECLARE @tags as nvarchar(max) = ''' + ','.join(section['tags']) +'''
+
+                INSERT INTO dataSections
+                (sID, Title, uID, Tags, GETDATE(), GETDATE())
+                VALUES
+                (@NEXTKEY, @title, @uid, @tags)
+                '''
+            run_the_real_execute_function(my_sql_1)
+
+            # Do the same check and insertion or update for the dataNoteTags
+
+            # Then insert the notes into dataNotes
+            my_sql_2 = ''' the real sql to insert each notes'''
+            run_the_real_execute_function(my_sql_1)
+"""
+
+# class UserInfo():
+    # def __init__(self):
+
+
+def build_json_string(userno):
+    '''
+    This function builds the JSON string from the DB for the current user.
+    '''
+
+    '''
+    0 -- id
+    1 -- body
+    2 -- user_id
+    3 -- notes
+    4 -- tags
+    '''
+
+    sql_sections = 'SELECT * FROM Section WHERE user_id = "{0}"'.format(userno)
+
+    con = db.engine.connect()
+    table = con.execute(sql_sections)
+    sections_rows = table.fetchall()
+
+    data_store = []
+
+    # Populate the DataStore with sections
+    for row in sections_rows:
+        cur_section = {}
+        cur_section.id = row[0]
+        cur_section.title = row[1]
+        cur_section.notes = []
+
+        sql_notes = '''
+            SELECT * 
+            FROM Note 
+            WHERE section_id = {0} 
+        ''' .format(cur_section.id)
+
+        con = db.engine.connect()
+        table = con.execute(sql_notes)
+        notes_rows = table.fetchall()
+
+        # Populate current section's notes array
+        for row in notes_rows:
+            cur_note = {}
+            cur_note.id = row[0]
+            cur_note.text = row[1]
+            cur_note.tags = []  # Default to empty right now
+
+            cur_section.notes.append(cur_note)
+        #end-for-notes
+
+        # Add this to the
+        data_store.append(cur_section)
+    #end-for-sections
+
+    return json.dumps(data_store)
