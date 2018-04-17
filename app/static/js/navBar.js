5ccc402e2b5611c67a8d0c693351b7defb0d1593
Uda Yeruultsengel <oyeruultsengel@etelligentsolutions.com>
2018-04-17 17:43:30 -0400

    UI cleanup & revert side menu click/view

--- app/static/js/navBar.js
+++ app/static/js/navBar.js
@@ -1,20 +1,18 @@
 /* Set the width of the side navigation to 250px */
-var sideNavWidth = 200;
 function openNav() {
+    var sideNavWidth = 0;
     if (window.innerHeight <= 450) {
-        sideNavWidth = 100;
+        sideNavWidth = "100px";
     }
     else {
-        sideNavWidth = 200;
+        sideNavWidth = "200px";
     }
-    $("#main-side-nav").css({"margin-left": 0});
-    $(".main-top-nav").css({"margin-left": sideNavWidth});
+    $("#main-side-nav").outerWidth(sideNavWidth);
     $("#main-content-container").css({"margin-left": sideNavWidth});
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    $("#main-side-nav").css({"margin-left": -1 * sideNavWidth});
-    $(".main-top-nav").css({"margin-left": 0});
+    $("#main-side-nav").outerWidth(0);
     $("#main-content-container").css({"margin-left": 0});
 }
